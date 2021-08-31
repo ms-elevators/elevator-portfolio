bdf4964a91dc79c3fcf132adef6b6e2bd7d333ba
koowoy <uk960214@gmail.com>
2021-08-31 22:29:04 +0900

    simplify door structure

--- src/containers/Door/Door.js
+++ src/containers/Door/Door.js
@@ -1,16 +1,21 @@
 import React from "react";
-
-// import components
-
-import DoorContent from "../../components/DoorContent";
+import { CSSTransition } from "react-transition-group";
 
 // import css
 import "./style.css";
 
 export default function Door({ floor, isReady }) {
   return (
-    <div className="Door">
-      <DoorContent className="content" floor={floor} isReady={isReady} />
+    <div className="Door door-container">
+      <div className="door-inner-content">
+        <h1>Current Floor: {floor}</h1>
+      </div>
+      <CSSTransition in={isReady} timeout={2000} classNames="door-left">
+        <div className="door-left"></div>
+      </CSSTransition>
+      <CSSTransition in={isReady} timeout={2000} classNames="door-right">
+        <div className="door-right"></div>
+      </CSSTransition>
     </div>
   );
 }

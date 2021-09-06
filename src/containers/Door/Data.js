4243b2bc4df4262153ce3b5798401d7defc0f72e
koowoy <uk960214@gmail.com>
2021-09-06 17:57:19 +0900

    routing complete

---
+++ src/containers/Door/Data.js
@@ -0,0 +1,42 @@
+import img1 from "../../image/img1.png";
+import img2 from "../../image/img2.png";
+import img3 from "../../image/img3.png";
+import img4 from "../../image/img4.png";
+
+export const content = {
+  1: {
+    img: [img1, img2, img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  2: {
+    img: [img2, img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  3: {
+    img: [img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  4: {
+    img: [img2, img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  5: {
+    img: [img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  6: {
+    img: [img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+  7: {
+    img: [img1, img2, img3, img4],
+    demo: "google.com",
+    code: "github.com",
+  },
+};

da1f1c429d3c56c5c097646ed795bb6d40d052f8
Seoyoung0613 <weseo0613@gmail.com>
2021-09-23 14:01:58 +0900

    door finished

--- src/containers/Door/Door.js
+++ src/containers/Door/Door.js
@@ -11,12 +11,13 @@ import {
   DoorLeft,
   DoorRight,
   Button,
-  Text,
   DoorFrameTop,
   DoorFrameBottom,
   DoorFrameLeft,
   DoorFrameRight,
+  IndicatorContainer,
   ContainerDot,
+  ImageBackground,
 } from "./Door.elements";
 
 import { content } from "./Data";
@@ -33,6 +34,7 @@ import bgi from "./office.jpg"; //Photo by <a href="https://unsplash.com/@sunday
 // isReady- App.js에서 받아온 것
 export default function Door({ floor, isReady, contactFloor }) {
   const [imgIdx, setImgIdx] = useState(0);
+  const [slideIdx, setSlideIdx] = useState(0);
 
   const previews = [];
   const imgLen = content[floor].img.length; // 층 수 별로 다른 이미지 개수
@@ -51,15 +53,18 @@ export default function Door({ floor, isReady, contactFloor }) {
   const handlePrev = () => {
     const prev = imgIdx === 0 ? imgLen - 1 : imgIdx - 1;
     setImgIdx(prev);
+    setSlideIdx(prev);
   };
 
   const handleNext = () => {
     const next = imgIdx === imgLen - 1 ? 0 : imgIdx + 1;
     setImgIdx(next);
+    setSlideIdx(next);
   };
 
-  const Dot = imgIdx => {
-    setSlideIndex(imgIdx)
+  const Dot = (imgIdx) => {
+    setImgIdx(imgIdx); // Dot 눌렀을 때 이미지 변환 여부를 결정 -> 해당 코드 없으면 Dot은 눌려도 이미지는 바뀌지 않음.
+    setSlideIdx(imgIdx); // Dot 눌림 여부를 결정
   };
 
   return (
@@ -75,22 +80,24 @@ export default function Door({ floor, isReady, contactFloor }) {
         </DoorInnerContent>
       ) : (
         <DoorInnerContent>
+        <ImageBackground>
           <ContentImgSection>
-            <Button onClick={() => handlePrev()}>＜</Button>
+            {/* 이전 버튼 */}
+            <Button onClick={() => handlePrev()}>＜</Button> 
             <ContentImgContainer>{previews}</ContentImgContainer>
+            {/* 다음 버튼 */}
             <Button onClick={() => handleNext()}>＞</Button>
           </ContentImgSection>
-          <Text>
-            {imgIdx + 1} / {imgLen}
-          </Text>
-          <ContainerDot>
-            {Array.from({length: imgLen}).map((item, imgIdx) => (
-                    <div 
-                    onClick={() => Dot(imgIdx + 1)}
-                    className={slideIndex === imgIdx + 1 ? "dotActive" : "dot"}
-                    ></div>
-              ))}
-          </ContainerDot>
+
+          <IndicatorContainer>
+            {Array.from({ length: imgLen }).map((item, imgIdx) => (
+              <ContainerDot
+                onClick={() => Dot(imgIdx)}
+                active={slideIdx === imgIdx ? "dotActive" : "dotInactive"}
+              ></ContainerDot>
+            ))}
+          </IndicatorContainer>
+        </ImageBackground>
 
           <ContentButtonContainer>
             <ContentLink href={content[floor].demo} target="_blank">

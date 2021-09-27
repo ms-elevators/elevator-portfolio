2646a01a116e9edcd7e2d2befa5cd15eff3cadc6
koowoy <uk960214@gmail.com>
2021-09-27 18:02:17 +0900

    speaker design update

--- src/components/Speaker/Speaker.elements.js
+++ src/components/Speaker/Speaker.elements.js
@@ -3,15 +3,59 @@ import styled from "styled-components";
 export const SpeakerContainer = styled.div`
   position: fixed;
   width: 10%;
-  top: 10%;
+  top: 8%;
   left: 8%;
 
-  i {
-    font-size: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 55%;
+    height: 55%;
   }
 `;
 
-export const SoundStateLabel = styled.p`
-  background-color: #e0e0e0;
-  border: solid 2px #000000;
+export const SoundStateContainer = styled.div`
+  width: 40%;
+
+  p,
+  i {
+    margin: 0;
+
+    color: #494949;
+    font-size: 0.7rem;
+  }
+`;
+
+export const SoundLED = styled.div`
+  width: 2vh;
+  padding-bottom: 1.5vh;
+  margin: 20% auto;
+
+  border-radius: 100%;
+  border: 2px solid ${(props) => (props.light === "on" ? "#00ff00" : "#ff0000")};
+
+  background-color: ${(props) =>
+    props.light === "on" ? "#00ff00ee" : "#ff0000ee"};
+  box-shadow: 0 0 3px 3px
+    ${(props) => (props.light === "on" ? "#00ff00aa" : "#ff0000aa")};
+`;
+
+export const SoundSwitch = styled.div`
+  width: 1.8vh;
+  padding-bottom: 1.3vh;
+  margin: 30% auto 10% auto;
+
+  border-radius: 100%;
+  border: 2px solid #aaaaaa;
+
+  background-color: #494949;
+  box-shadow: 0 0 3px 1px #333333ef;
+
+  cursor: pointer;
+
+  &:active {
+    box-shadow: none;
+  }
 `;
